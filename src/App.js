addb20dd4950a2ad42337601e7e95eb54604d53d
Evi Edor <evi-edor@Evi.local>
2022-06-04 23:59:57 -0400

    updating app with its component jsx and scss files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import { Component } from "react";
-import "./App.css";
+import Directory from "./components/directory/directory.component";
 
+const App = () => {
+	// creating the model to reflect the apps object properties
+	const categories = [
+		{
+			id: 1,
+			title: "hat",
+			imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+		},
+		{
+			id: 2,
+			title: "jacket",
+			imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+		},
+		{
+			id: 3,
+			title: "sneaker",
+			imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+		},
+		{
+			id: 4,
+			title: "women",
+			imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+		},
+		{
+			id: 5,
+			title: "men",
+			imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+		},
+	];
 
-class App extends Component {
-	constructor() {}
-
-	componentDidMount() {}
-
-	render() {
-		return
-	}
-}
+	return <Directory categories={categories} />;
+};
 
 export default App;
