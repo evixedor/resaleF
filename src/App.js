f7ddb6a07a3c936e00c609dc2be357168d9870ae
Evi Edor <evi-edor@Evi.local>
2022-06-04 21:11:51 -0400

    added application folder structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+
+
+class App extends Component {
+	constructor() {}
+
+	componentDidMount() {}
+
+	render() {
+		return
+	}
+}
+
+export default App;
