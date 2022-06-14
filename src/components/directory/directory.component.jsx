46a4ff7121d629c92c5ddd75afb4b05555b5deb4
Work <work@rwa.local>
2022-06-14 15:44:16 -0400

    updating app with styled components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -7,31 +7,31 @@ const categories = [
     id: 1,
     title: "hat",
     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    routeName: 'shop/hat'
+    routeName: 'shop/hats'
   },
   {
     id: 2,
     title: "jacket",
     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    routeName: 'shop/jacket'
+    routeName: 'shop/jackets'
   },
   {
     id: 3,
     title: "sneaker",
     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    routeName: 'shop/sneaker'
+    routeName: 'shop/sneakers'
   },
   {
     id: 4,
     title: "women",
     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    routeName: 'shop/women'
+    routeName: 'shop/womens'
   },
   {
     id: 5,
     title: "men",
     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    routeName: 'shop/men'
+    routeName: 'shop/mens'
   },
 ];
 
